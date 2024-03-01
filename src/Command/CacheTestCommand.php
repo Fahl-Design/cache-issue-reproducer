3f54a81991ec5c73acce0b9c52e41a72aeb10d2b
Benjamin Fahl <git@fahl-design.de>
2024-03-01 21:03:56 +0100

    make issue more visible, update to newest symfony

--- src/Command/CacheTestCommand.php
+++ src/Command/CacheTestCommand.php
@@ -11,8 +11,6 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
-use function uniqid;
-
 #[AsCommand(
     name: 'app:cache-test',
     description: 'Add a short description for your command',
@@ -38,49 +36,55 @@ class CacheTestCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+        $addTags = $input->getOption('add-tags');
+        $counter = 0;
+        $key = 'myfancy_cache_key_'.($addTags ? 'with_tags_' : '');
+        $key .= \uniqid('key', false);
+        $io->section('cache key: '.$key);
 
-        if ($input->getOption('add-tags')) {
+        if ($addTags) {
             $io->writeln('[+] now with tags #1');
-            $io->writeln($this->getDataFromCacheWithTags($io));
+            $io->writeln($this->getDataFromCacheWithTags($io, $key, $counter));
             $io->writeln('[+] now with tags #2');
-            $io->writeln($this->getDataFromCacheWithTags($io));
+            $io->writeln($this->getDataFromCacheWithTags($io, $key, $counter));
             $io->writeln('[+] now with tags #3');
-            $io->writeln($this->getDataFromCacheWithTags($io));
+            $io->writeln($this->getDataFromCacheWithTags($io, $key, $counter));
         } else {
             $io->writeln('[+] no tags #1');
-            $io->writeln($this->getDataFromCacheWithoutTags($io));
+            $io->writeln($this->getDataFromCacheWithoutTags($io, $key, $counter));
             $io->writeln('[+] no tags #2');
-            $io->writeln($this->getDataFromCacheWithoutTags($io));
+            $io->writeln($this->getDataFromCacheWithoutTags($io, $key, $counter));
             $io->writeln('[+] no tags #3');
-            $io->writeln($this->getDataFromCacheWithoutTags($io));
+            $io->writeln($this->getDataFromCacheWithoutTags($io, $key, $counter));
         }
 
         return Command::SUCCESS;
     }
 
-    private function getDataFromCacheWithTags(SymfonyStyle $io): string
+    private function getDataFromCacheWithTags(SymfonyStyle $io, string $key, int &$counter): string
     {
         return $this->fancyCachePool->get(
-            key: 'fancy_cache_key_3',
-            callback: function (ItemInterface $item) use ($io): string {
-                $io->info('cache callback was hit to build cache with tags');
+            key: $key,
+            callback: function (ItemInterface $item) use (&$counter, $io): string {
+                ++$counter;
+                $returnValue = \uniqid('cached_value', true);
+                $io->success('cache callback was hit to build cache with tags: '.$counter.' added value: '.$returnValue);
                 $item->tag(['tag1', 'tag2']);
 
-                return 'my cached value';
+                return 'my cached value with tags_'.$counter.'_'.$returnValue;
             });
     }
 
-    private function getDataFromCacheWithoutTags(SymfonyStyle $io): string
+    private function getDataFromCacheWithoutTags(SymfonyStyle $io, string $key, int &$counter): string
     {
         return $this->fancyCachePool->get(
-            key: 'fancy_cache_key_4',
-            callback: function (ItemInterface $item) use ($io): string {
-                dump($item->isHit() ? 'hit' : 'no hit');
-                $io->info('cache callback was hit to build no tags');
-//                $item->tag(['tag1', 'tag2']);
+            key: $key,
+            callback: function () use (&$counter, $io): string {
+                ++$counter;
+                $returnValue = \uniqid('cached_value', true);
+                $io->success('cache callback was hit to build no tags: '.$counter.' added value: '.$returnValue);
 
-                return 'my cached value';
+                return 'my cached value_'.$counter.'_'.$returnValue;
             });
     }
-
 }
